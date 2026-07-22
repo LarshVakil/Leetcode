5376f6beea1ebabce2cca1dcc85192c258dbc5b3
Larsh Vakil <larshvakil01@gmail.com>
2026-07-22 05:31:22 +0530

    Time: 0 ms (100%), Space: 19.2 MB (86.2%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,17 +1,33 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        #o(n^2) soln 
-        p = 0
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i < j:
-                    if nums[i] == nums[j]:
-                        p += 1
-                    else:
-                        pass
-                else:
-                    pass
+        # #o(n^2) soln 
+        # p = 0
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if i < j:
+        #             if nums[i] == nums[j]:
+        #                 p += 1
+        #             else:
+        #                 pass
+        #         else:
+        #             pass
 
-        return p 
-                
+        # return p 
+
+        # o(n) - Hashmap 
+        count = {}
+        goodpairs =0
+
+        for i in nums:
+            if i in count:
+                goodpairs += count[i]
+                count[i] += 1
+            else:
+                count[i] = 1
+        return goodpairs
+
+
+        
+
+
                     
